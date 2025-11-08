110a311d754bbbfae465d086d600ab3894c9c757
NoroiMusha37 <vo.melnyk37@gmail.com>
2025-11-08 18:29:51 +0200

    add TaskListView

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import index
+from .views import TaskListView
 
 urlpatterns = [
-    path("", index, name="index"),
+    path("", TaskListView.as_view(), name="index"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Task, Tag
 
-# Create your views here.
+
+class TaskListView(generic.ListView):
+    model = Task
+    context_object_name = "tasks_list"
+    template_name = "todo/index.html"
